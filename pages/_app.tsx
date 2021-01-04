c0cbefb06ed8cef7b8cc3bc8ddaecd85ed981f33
Leigh Halliday <leighhalliday@gmail.com>
2021-01-04 13:53:25 -0500

    04 creating layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import { AppProps } from "next/app";
-// import Head from "next/head";
+import Head from "next/head";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
 // import { AuthProvider } from "src/auth/useAuth";
-// import "../styles/index.css";
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Home Sweet Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,45 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import { FunctionComponent, ReactNode } from "react";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+interface IProps {
+  main: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ main }) => {
+  const authenticated = false;
+  const logout = () => null;
+
+  return (
+    <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
+      <nav className="bg-gray-800" style={{ height: "64px" }}>
+        <div className="px-6 flex items-center justify-between h-16">
+          <Link href="/">
+            <a>
+              <img
+                src="/home-color.svg"
+                alt="home house"
+                className="inline w-6"
+              />
+            </a>
+          </Link>
+          {authenticated ? (
+            <>
+              <Link href="/houses/add">
+                <a>Add House</a>
+              </Link>
+              <button onClick={logout}>Logout</button>
+            </>
+          ) : (
+            <Link href="/auth">
+              <a>Login / Signup</a>
+            </Link>
+          )}
+        </div>
+      </nav>
+      <main style={{ minHeight: "calc(100vh - 64px)" }}>{main}</main>
+    </div>
+  );
+};
+
+export default Layout;
