9a937df39d8865641776a7dd55a5d79338c37ee2
Leigh Halliday <leighhalliday@gmail.com>
2021-01-05 08:23:27 -0500

    06 auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,20 @@
-// import Layout from "src/components/layout";
-// import FirebaseAuth from "src/components/firebaseAuth";
-// import { GetServerSideProps, NextApiRequest } from "next";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
+import Layout from "src/components/layout";
+import FirebaseAuth from "src/components/firebaseAuth";
+import { GetServerSideProps, NextApiRequest } from "next";
+import { loadIdToken } from "src/auth/firebaseAdmin";
 
 export default function Auth() {
-  return <div>Auth</div>;
+  return <Layout main={<FirebaseAuth />} />;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const uid = await loadIdToken(req as NextApiRequest);
+
+  if (uid) {
+    res.setHeader("location", "/");
+    res.statusCode = 302;
+    res.end();
+  }
+
+  return { props: {} };
+};
--- src/components/firebaseAuth.tsx
+++ src/components/firebaseAuth.tsx
@@ -1,6 +1,36 @@
-// import { FunctionComponent, useState, useEffect } from "react";
-// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-// import firebase from "firebase/app";
-// import "firebase/auth";
+import { FunctionComponent, useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-export {};
+const firebaseAuthConfig = {
+  signInFlow: "popup",
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false,
+    },
+  ],
+  signInSuccessUrl: "/",
+};
+
+const FirebaseAuth: FunctionComponent = () => {
+  const [renderAuth, setRenderAuth] = useState(false);
+
+  useEffect(() => {
+    setRenderAuth(true);
+  }, []);
+
+  return (
+    <div className="mt-16">
+      {renderAuth ? (
+        <StyledFirebaseAuth
+          uiConfig={firebaseAuthConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default FirebaseAuth;
