e407f66a94cfe4dc7defb5dc784bde4c9757d0cd
Leigh Halliday <leighhalliday@gmail.com>
2021-01-05 09:09:05 -0500

    08 init house form

--- pages/houses/add.tsx
+++ pages/houses/add.tsx
@@ -1,8 +1,20 @@
-// import { GetServerSideProps, NextApiRequest } from "next";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
-// import Layout from "src/components/layout";
-// import HouseForm from "src/components/houseForm";
+import { GetServerSideProps, NextApiRequest } from "next";
+import { loadIdToken } from "src/auth/firebaseAdmin";
+import Layout from "src/components/layout";
+import HouseForm from "src/components/houseForm";
 
 export default function Add() {
-  return <div>Add</div>;
+  return <Layout main={<HouseForm />} />;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const uid = await loadIdToken(req as NextApiRequest);
+
+  if (!uid) {
+    res.setHeader("location", "/auth");
+    res.statusCode = 302;
+    res.end();
+  }
+
+  return { props: {} };
+};
--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,5 +1,5 @@
-// import { useState, useEffect, ChangeEvent } from "react";
-// import { useForm } from "react-hook-form";
+import { useState, useEffect, ChangeEvent } from "react";
+import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
 // import Link from "next/link";
@@ -15,4 +15,46 @@
 // } from "src/generated/UpdateHouseMutation";
 // import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
 
-export {};
+interface IFormData {
+  address: string;
+  latitude: number;
+  longitude: number;
+  bedrooms: string;
+  image: FileList;
+}
+
+interface IProps {}
+
+export default function HouseForm({}: IProps) {
+  const [submitting, setSubmitting] = useState(false);
+  const { register, handleSubmit, setValue, errors, watch } = useForm<
+    IFormData
+  >({ defaultValues: {} });
+
+  useEffect(() => {
+    register({ name: "address" }, { required: "Please enter your address" });
+    register({ name: "latitude" }, { required: true, min: -90, max: 90 });
+    register({ name: "longitude" }, { required: true, min: -180, max: 180 });
+  }, [register]);
+
+  const handleCreate = async (data: IFormData) => {};
+
+  const onSubmit = (data: IFormData) => {
+    setSubmitting(false);
+    handleCreate(data);
+  };
+
+  return (
+    <form className="mx-auto max-w-xl py-4" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="text-xl">Add a New House</h1>
+
+      <div className="mt-4">
+        <label htmlFor="search" className="block">
+          Search for your address
+        </label>
+        {/* SEARCH FIELD */}
+        {errors.address && <p>{errors.address.message}</p>}
+      </div>
+    </form>
+  );
+}
