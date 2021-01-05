8db36f3f3483c19faafff2071304f91ecb370645
Leigh Halliday <leighhalliday@gmail.com>
2021-01-05 08:41:39 -0500

    07 home map

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,28 @@
 // import { useQuery, gql } from "@apollo/client";
 // import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
-// import Map from "src/components/map";
+import Map from "src/components/map";
 // import HouseList from "src/components/houseList";
 // import { useLastData } from "src/utils/useLastData";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
 
 export default function Home() {
-  return <Layout main={<div>Home</div>} />;
+  return (
+    <Layout
+      main={
+        <div className="flex">
+          <div
+            className="w-1/2 pb-4"
+            style={{ maxHeight: "calc(100vh - 64px)", overflowX: "scroll" }}
+          >
+            HouseList
+          </div>
+          <div className="w-1/2">
+            <Map />
+          </div>
+        </div>
+      }
+    />
+  );
 }
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,10 +1,35 @@
-// import { useRef, useState } from "react";
-// import Link from "next/link";
-// import { Image } from "cloudinary-react";
-// import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
+import { useRef, useState } from "react";
+import Link from "next/link";
+import { Image } from "cloudinary-react";
+import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery_houses } from "src/generated/HousesQuery";
 // import { SearchBox } from "./searchBox";
 
-export {};
+interface IProps {}
+
+export default function Map({}: IProps) {
+  const mapRef = useRef<ReactMapGL | null>(null);
+  const [viewport, setViewport] = useState<ViewState>({
+    latitude: 43,
+    longitude: -79,
+    zoom: 10,
+  });
+
+  return (
+    <div className="text-black relative">
+      <ReactMapGL
+        {...viewport}
+        width="100%"
+        height="calc(100vh - 64px)"
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        ref={(instance) => (mapRef.current = instance)}
+        minZoom={5}
+        maxZoom={15}
+        mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
+      ></ReactMapGL>
+    </div>
+  );
+}
