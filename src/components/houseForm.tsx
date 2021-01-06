e192f5d7f5ee04fcd7b5deb14ce8be7de3775d0a
Leigh Halliday <leighhalliday@gmail.com>
2021-01-06 08:25:06 -0500

    09 add house search form

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 // import { useRouter } from "next/router";
 // import Link from "next/link";
 // import { Image } from "cloudinary-react";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 // import {
 //   CreateHouseMutation,
 //   CreateHouseMutationVariables,
@@ -30,6 +30,7 @@ export default function HouseForm({}: IProps) {
   const { register, handleSubmit, setValue, errors, watch } = useForm<
     IFormData
   >({ defaultValues: {} });
+  const address = watch("address");
 
   useEffect(() => {
     register({ name: "address" }, { required: "Please enter your address" });
@@ -52,8 +53,16 @@ export default function HouseForm({}: IProps) {
         <label htmlFor="search" className="block">
           Search for your address
         </label>
-        {/* SEARCH FIELD */}
+        <SearchBox
+          onSelectAddress={(address, latitude, longitude) => {
+            setValue("address", address);
+            setValue("latitude", latitude);
+            setValue("longitude", longitude);
+          }}
+          defaultValue=""
+        />
         {errors.address && <p>{errors.address.message}</p>}
+        <h2>{address}</h2>
       </div>
     </form>
   );
--- src/components/searchBox.tsx
+++ src/components/searchBox.tsx
@@ -1,17 +1,95 @@
-// import { ChangeEvent } from "react";
-// import { FunctionComponent } from "react";
-// import usePlacesAutocomplete, {
-//   getGeocode,
-//   getLatLng,
-// } from "use-places-autocomplete";
-// import { useGoogleMapsScript, Libraries } from "use-google-maps-script";
-// import {
-//   Combobox,
-//   ComboboxInput,
-//   ComboboxPopover,
-//   ComboboxList,
-//   ComboboxOption,
-// } from "@reach/combobox";
-// import "@reach/combobox/styles.css";
+import { ChangeEvent } from "react";
+import { FunctionComponent } from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import { useGoogleMapsScript, Libraries } from "use-google-maps-script";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
-export {};
+interface ISearchBoxProps {
+  onSelectAddress: (
+    address: string,
+    latitude: number | null,
+    longitude: number | null
+  ) => void;
+  defaultValue: string;
+}
+
+const libraries: Libraries = ["places"];
+
+export function SearchBox({ onSelectAddress, defaultValue }: ISearchBoxProps) {
+  const { isLoaded, loadError } = useGoogleMapsScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "",
+    libraries,
+  });
+
+  if (!isLoaded) return null;
+  if (loadError) return <div>Error loading</div>;
+
+  return (
+    <ReadySearchBox
+      onSelectAddress={onSelectAddress}
+      defaultValue={defaultValue}
+    />
+  );
+}
+
+function ReadySearchBox({ onSelectAddress, defaultValue }: ISearchBoxProps) {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete({ debounce: 300, defaultValue });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    if (e.target.value === "") {
+      onSelectAddress("", null, null);
+    }
+  };
+
+  const handleSelect = async (address: string) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      onSelectAddress(address, lat, lng);
+    } catch (error) {
+      console.error(`😱 Error:`, error);
+    }
+  };
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        id="search"
+        value={value}
+        onChange={handleChange}
+        disabled={!ready}
+        placeholder="Search your location"
+        className="w-full p-2"
+        autoComplete="off"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+}
