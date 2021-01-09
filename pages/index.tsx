51e0300e4f47073f1b80ea13e454e622f3e245ee
Leigh Halliday <leighhalliday@gmail.com>
2021-01-09 08:20:48 -0500

    20 track map bounds

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
 // import { useState } from "react";
 // import { useQuery, gql } from "@apollo/client";
-// import { useDebounce } from "use-debounce";
+import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
 import Map from "src/components/map";
 // import HouseList from "src/components/houseList";
 // import { useLastData } from "src/utils/useLastData";
-// import { useLocalState } from "src/utils/useLocalState";
+import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
 
+type BoundsArray = [[number, number], [number, number]];
+
 export default function Home() {
+  const [dataBounds, setDataBounds] = useLocalState<string>(
+    "bounds",
+    "[[0,0],[0,0]]"
+  );
+  const [debouncedDataBounds] = useDebounce(dataBounds, 200);
+
   return (
     <Layout
       main={
@@ -20,7 +28,7 @@ export default function Home() {
             HouseList
           </div>
           <div className="w-1/2">
-            <Map />
+            <Map setDataBounds={setDataBounds} />
           </div>
         </div>
       }
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -7,9 +7,11 @@ import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery_houses } from "src/generated/HousesQuery";
 // import { SearchBox } from "./searchBox";
 
-interface IProps {}
+interface IProps {
+  setDataBounds: (bounds: string) => void;
+}
 
-export default function Map({}: IProps) {
+export default function Map({ setDataBounds }: IProps) {
   const mapRef = useRef<ReactMapGL | null>(null);
   const [viewport, setViewport] = useLocalState<ViewState>("viewport", {
     latitude: 43,
@@ -29,6 +31,18 @@ export default function Map({}: IProps) {
         minZoom={5}
         maxZoom={15}
         mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
+        onLoad={() => {
+          if (mapRef.current) {
+            const bounds = mapRef.current.getMap().getBounds();
+            setDataBounds(JSON.stringify(bounds.toArray()));
+          }
+        }}
+        onInteractionStateChange={(extra) => {
+          if (!extra.isDragging && mapRef.current) {
+            const bounds = mapRef.current.getMap().getBounds();
+            setDataBounds(JSON.stringify(bounds.toArray()));
+          }
+        }}
       ></ReactMapGL>
     </div>
   );
