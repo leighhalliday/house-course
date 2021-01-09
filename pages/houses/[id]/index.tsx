b44bcd2bf1be68ddc6adc451578d7611d92861a5
Leigh Halliday <leighhalliday@gmail.com>
2021-01-09 07:18:27 -0500

    17 single map

--- pages/houses/[id]/index.tsx
+++ pages/houses/[id]/index.tsx
@@ -3,7 +3,7 @@ import { Image } from "cloudinary-react";
 import { useQuery, gql } from "@apollo/client";
 import Layout from "src/components/layout";
 // import HouseNav from "src/components/houseNav";
-// import SingleMap from "src/components/singleMap";
+import SingleMap from "src/components/singleMap";
 import {
   ShowHouseQuery,
   ShowHouseQueryVariables,
@@ -67,7 +67,9 @@ function HouseData({ id }: { id: string }) {
 
             <p>{house.bedrooms} 🛌 house</p>
           </div>
-          <div className="sm:w-full md:w-1/2">SingleMap</div>
+          <div className="sm:w-full md:w-1/2">
+            <SingleMap house={house} />
+          </div>
         </div>
       }
     />
--- src/components/singleMap.tsx
+++ src/components/singleMap.tsx
@@ -1,6 +1,55 @@
-// import { useState } from "react";
-// import Link from "next/link";
-// import ReactMapGL, { Marker, NavigationControl } from "react-map-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
+import { useState } from "react";
+import Link from "next/link";
+import ReactMapGL, { Marker, NavigationControl } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-export {};
+interface IHouse {
+  id: string;
+  latitude: number;
+  longitude: number;
+}
+
+interface IProps {
+  house: IHouse;
+}
+
+export default function SingleMap({ house }: IProps) {
+  const [viewport, setViewport] = useState({
+    latitude: house.latitude,
+    longitude: house.longitude,
+    zoom: 13,
+  });
+
+  return (
+    <div className="text-black">
+      <ReactMapGL
+        {...viewport}
+        width="100%"
+        height="calc(100vh - 64px)"
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+        mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
+        scrollZoom={false}
+        minZoom={8}
+      >
+        <div className="absolute top-0 left-0 p-4">
+          <NavigationControl showCompass={false} />
+        </div>
+
+        <Marker
+          latitude={house.latitude}
+          longitude={house.longitude}
+          offsetLeft={-15}
+          offsetTop={-15}
+        >
+          <button
+            type="button"
+            style={{ width: "30px", height: "30px", fontSize: "30px" }}
+          >
+            <img src="/home-color.svg" className="w-8" alt="selected house" />
+          </button>
+        </Marker>
+      </ReactMapGL>
+    </div>
+  );
+}
