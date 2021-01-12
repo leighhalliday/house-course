4dc49d501a6d1c0067eb9dab7d109b6e77f1aa4f
Leigh Halliday <leighhalliday@gmail.com>
2021-01-12 06:31:25 -0500

    29 bonus auth token cookie

--- src/auth/tokenCookies.ts
+++ src/auth/tokenCookies.ts
@@ -1,11 +1,19 @@
-import cookies from "js-cookie";
-
-export const getTokenCookie = () => cookies.get("token");
-
 export const setTokenCookie = (token: string) => {
-  cookies.set("token", token, {
-    expires: 1 / 24,
+  fetch("/api/login", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ token }),
   });
 };
 
-export const removeTokenCookie = () => cookies.remove("token");
+export const removeTokenCookie = () => {
+  fetch("/api/logout", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+};
