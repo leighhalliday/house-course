e004e8cdafb309f5557be64d5f1c3af649d0cb9d
Sezayi Arslan <arslan.sezayi@gmail.com>
2021-02-04 20:25:20 +0100

    add layout component and styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 // import { useState } from "react";
 // import { useQuery, gql } from "@apollo/client";
 // import { useDebounce } from "use-debounce";
-// import Layout from "src/components/layout";
+import Layout from "src/components/layout";
 // import Map from "src/components/map";
 // import HouseList from "src/components/houseList";
 // import { useLastData } from "src/utils/useLastData";
@@ -9,5 +9,5 @@
 // import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
 
 export default function Home() {
-  return <div>Home</div>;
+  return <Layout main={<div>Home</div>} />;
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,54 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import { FunctionComponent, ReactNode } from "react";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+interface IProps {
+  main: ReactNode;
+}
+
+const Layout: FunctionComponent<IProps> = ({ main }) => {
+  const authenticated = false;
+
+  const logout = () => null;
+
+  return (
+    <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
+      <nav className="bg-gray-900 " style={{ height: "80px" }}>
+        <div className="px-8 flex items-center justify-between h-16">
+          <div>
+            <Link href="/">
+              <a>
+                <img
+                  src="/ss-logo.svg"
+                  alt="Sportyspots logo"
+                  className="inline w-40"
+                />
+              </a>
+            </Link>
+          </div>
+
+          <div>
+            {authenticated ? (
+              <>
+                <Link href="/spots/add">
+                  <a>add a spot</a>
+                </Link>
+                <button style={{ marginLeft: "16px" }} onClick={logout}>
+                  Logout
+                </button>{" "}
+              </>
+            ) : (
+              <Link href="/auth">
+                <a>Login / Signup</a>
+              </Link>
+            )}
+          </div>
+        </div>
+      </nav>
+
+      <main style={{ minHeight: "calc(100vh - 80px)" }}>{main}</main>
+    </div>
+  );
+};
+
+export default Layout;
