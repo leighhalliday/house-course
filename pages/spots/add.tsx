2a1b3ef7a53838c4c1ed21924c4ba27a1db32417
Sezayi Arslan <arslan.sezayi@gmail.com>
2021-02-17 16:19:53 +0100

    added spotform

--- pages/spots/add.tsx
+++ pages/spots/add.tsx
@@ -1,8 +1,20 @@
-// import { GetServerSideProps, NextApiRequest } from "next";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
-// import Layout from "src/components/layout";
-// import HouseForm from "src/components/houseForm";
+import { GetServerSideProps, NextApiRequest } from "next";
+import { loadIdToken } from "src/auth/firebaseAdmin";
+import Layout from "src/components/layout";
+import SpotForm from "src/components/spotForm";
 
 export default function Add() {
-  return <div>Add</div>;
+  return <Layout main={<SpotForm />} />;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const uid = await loadIdToken(req as NextApiRequest);
+
+  if (!uid) {
+    res.setHeader("location", "/auth");
+    res.statusCode = 302;
+    res.end();
+  }
+
+  return { props: {} };
+};
---
+++ src/components/spotForm.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect, ChangeEvent } from "react";
+import { useForm } from "react-hook-form";
+// import { useMutation, gql } from "@apollo/client";
+// import { useRouter } from "next/router";
+// import Link from "next/link";
+// import { Image } from "cloudinary-react";
+// import { SearchBox } from "./searchBox";
+// import {
+//   CreateHouseMutation,
+//   CreateHouseMutationVariables,
+// } from "src/generated/CreateHouseMutation";
+// import {
+//   UpdateHouseMutation,
+//   UpdateHouseMutationVariables,
+// } from "src/generated/UpdateHouseMutation";
+// import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
+
+interface IFormData {
+  address: string;
+  latitude: number;
+  longitude: number;
+  sport: string;
+  image: FileList;
+}
+
+interface IProps {}
+
+export default function SpotForm({}: IProps) {
+  const [submitting, setSubmitting] = useState(false);
+  const { register, handleSubmit, setValue, errors, watch } = useForm<
+    IFormData
+  >({ defaultValues: {} });
+
+  useEffect(() => {
+    register({ name: "address" }, { required: "Please enter an address" });
+    register({ name: "latitude" }, { required: true, min: -90, max: 90 });
+    register({ name: "longitude" }, { required: true, min: -180, max: 180 });
+  }, [register]);
+
+  const handleCreate = async (data: IFormData) => {};
+  const onSubmit = (data: IFormData) => {
+    setSubmitting(true);
+    handleCreate(data);
+  };
+  return (
+    <form className="mx-auto max-w-xl py-4" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="text-xl">Add a New Spot</h1>
+      <div className="mt-4">
+        <label htmlFor="search" className="block">
+          Search for your address
+        </label>
+        {/* SEARCH FIELD*/}
+        {errors.address && <p>{errors.address.message}</p>}
+      </div>
+    </form>
+  );
+}
