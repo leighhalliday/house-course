a9a1cc83f5681780b648e9af3c4b1ab37ab21bb3
Sezayi Arslan <arslan.sezayi@gmail.com>
2021-02-17 15:55:24 +0100

    add mapbox plus map layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,8 +10,8 @@ const Layout: FunctionComponent<IProps> = ({ main }) => {
   const { logout, authenticated } = useAuth();
 
   return (
-    <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
-      <nav className="bg-gray-900 " style={{ height: "80px" }}>
+    <div className="bg-gray-900 max-w-screen-3xl mx-auto text-white">
+      <nav className="bg-gray-900 " style={{ height: "64px" }}>
         <div className="px-8 flex items-center justify-between h-16">
           <div>
             <Link href="/">
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,10 +1,35 @@
-// import { useRef, useState } from "react";
-// import Link from "next/link";
-// import { Image } from "cloudinary-react";
-// import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
+import { useRef, useState } from "react";
+import Link from "next/link";
+import { Image } from "cloudinary-react";
+import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery_houses } from "src/generated/HousesQuery";
 // import { SearchBox } from "./searchBox";
 
-export {};
+interface IProps {}
+
+export default function Map({}: IProps) {
+  const mapRef = useRef<ReactMapGL | null>(null);
+  const [viewport, setViewport] = useState<ViewState>({
+    latitude: 52.379189,
+    longitude: 4.899431,
+    zoom: 12,
+  });
+
+  return (
+    <div className="text-black relative">
+      <ReactMapGL
+        {...viewport}
+        width="100%"
+        height="calc(100vh - 64px)"
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        ref={(instance) => (mapRef.current = instance)}
+        minZoom={10}
+        maxZoom={15}
+        mapStyle={"mapbox://styles/sezayi/ckkjxz1uw2a9017nwzr3wfimk"}
+      ></ReactMapGL>
+    </div>
+  );
+}
