02d4b0d63494a2cbb0ad6ef3a61f867fdf8a1983
Sezayi Arslan <arslan.sezayi@gmail.com>
2021-02-18 19:52:00 +0100

    add prisma

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,7 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-export {};
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn"],
+});
+
+export { prisma, PrismaClient };
