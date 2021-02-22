bb1cc445584f3e524a81246909b55635828b394a
Sezayi Arslan <arslan.sezayi@gmail.com>
2021-02-22 21:44:40 +0100

    add uselocalstate hook

--- src/utils/useLocalState.ts
+++ src/utils/useLocalState.ts
@@ -1,3 +1,21 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-export {};
+export function useLocalState<S = undefined>(key: string, initial: S) {
+  const [value, setValue] = useState<S>(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const saved = window.localStorage.getItem(key);
+      if (saved) {
+        return JSON.parse(saved);
+      }
+    }
+    return initial;
+  });
+
+  useEffect(() => {
+    if (window.localStorage) {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [value]);
+
+  return [value, setValue] as [typeof value, typeof setValue];
+}
