378b18819fbdb494f4a0644cd792cabaf7a49f37
Sezayi Arslan <arslan.sezayi@gmail.com>
2021-04-02 15:53:17 +0200

    add new modal with react portal

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps;
+    return { ...initialProps };
   }
 
   render() {
@@ -49,6 +49,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div id="modal-root"></div>
         </body>
       </Html>
     );
--- src/components/spotReview.tsx
+++ src/components/spotReview.tsx
@@ -3,13 +3,13 @@ import { FaStar } from "react-icons/fa";
 import { useRouter } from "next/router";
 import { useQuery, useMutation, gql } from "@apollo/client";
 import { loadIdToken } from "src/auth/firebaseAdmin";
-import Layout from "src/components/layout";
 import { useAuth } from "src/auth/useAuth";
 import {
   createSpotReview,
   createSpotReviewVariables,
 } from "src/generated/createSpotReview";
 import { ShowSpotQuery_spot } from "src/generated/ShowSpotQuery";
+import Modal from "src/components/modal";
 
 const ADD_SPOTREVIEW = gql`
   mutation createSpotReview($input: SpotReviewInput!) {
@@ -33,25 +33,16 @@ const SpotReview: React.FC<{ spot: ShowSpotQuery_spot }> = ({ spot }) => {
   const [rating, setRating] = useState<number>(0);
   const [hover, setHover] = useState<number>(0);
   const [disable, setDisable] = useState<boolean>(false);
-
-  console.log(hover);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const [mutate, status] = useMutation<
     createSpotReview,
     createSpotReviewVariables
   >(ADD_SPOTREVIEW);
 
-  //status:
-  //Mutation result:
-  // Property	Type	Description
-  // data	TData	The data returned from your mutation. It can be undefined if ignoreResults is true.
-  // loading	boolean	A boolean indicating whether your mutation is in flight
-  // error	ApolloError	Any errors returned from the mutation
-  // called	boolean	A boolean indicating if the mutate function has been called
-  // client	ApolloClient	Your ApolloClient instance. Useful for invoking cache methods outside the context of the update function, such as client.writeData and client.readQuery.
-
   const addRating = (newRating: number) => {
     setDisable(true);
+    setIsOpen(true);
     mutate({
       variables: {
         input: {
@@ -62,16 +53,12 @@ const SpotReview: React.FC<{ spot: ShowSpotQuery_spot }> = ({ spot }) => {
       },
     })
       .then(() => {
-        alert("Thanks for submitting your review!");
+        ("");
       })
       .catch((e) => {
         alert("Oops, something went wrong. Try refreshing the page.");
       });
   };
-
-  console.log(disable);
-
-  // const reviewCount = rating.length;
 
   const reviewAverage: number =
     spot.reviews.length === 0
@@ -81,54 +68,59 @@ const SpotReview: React.FC<{ spot: ShowSpotQuery_spot }> = ({ spot }) => {
   const roundedReviewAverage: string = reviewAverage.toFixed(1);
 
   return (
-    <div className="flex items-center ml-auto">
-      <p className="pr-4">Review this spot:</p>
-      {[...Array(5)].map((star, i) => {
-        const ratingValue = i + 1;
+    <>
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        Thanks for submitting your review
+      </Modal>
 
-        return (
-          <label key={i}>
-            <input
-              className="hidden"
-              type="radio"
-              name="rating"
-              value={ratingValue}
-              onClick={() => setRating(ratingValue)}
-            />
+      <div className="flex items-center ml-auto">
+        <p className="pr-4">Review this spot:</p>
+        {[...Array(5)].map((star, i) => {
+          const ratingValue = i + 1;
 
-            <FaStar
-              className="cursor-pointer"
-              size={20}
-              color={ratingValue <= (hover || rating) ? "#ffc107" : "e4e5e9"}
-              onMouseEnter={() => setHover(ratingValue)}
-              onMouseLeave={() => setHover(0)}
-            />
-          </label>
-        );
-      })}
-      <div className="pl-4">
-        <button
-          className={
-            rating === 0
-              ? " cursor-not-allowed border-style: none; bg-gray-500 shadow-md text-sm text-white font-bold py-0 md:px-4 px-4 py-2 rounded uppercase  focus:outline-none"
-              : "cursor-pointer bg-green-800 shadow-md text-sm text-white font-bold py-0 md:px-4 px-4 py-2 hover:bg-green-600 rounded uppercase"
-          }
-          onClick={
-            rating > 0
-              ? () => addRating(rating)
-              : () => alert("Please select a rating before submitting")
-          }
-        >
-          Submit review
-        </button>
+          return (
+            <label key={i}>
+              <input
+                className="hidden"
+                type="radio"
+                name="rating"
+                value={ratingValue}
+                onClick={() => setRating(ratingValue)}
+              />
+
+              <FaStar
+                className="cursor-pointer"
+                size={20}
+                color={ratingValue <= (hover || rating) ? "#ffc107" : "e4e5e9"}
+                onMouseEnter={() => setHover(ratingValue)}
+                onMouseLeave={() => setHover(0)}
+              />
+            </label>
+          );
+        })}
+        <div className="pl-4">
+          <button
+            className={
+              rating === 0
+                ? " cursor-not-allowed border-style: none; bg-gray-500 shadow-md text-sm text-white font-bold py-0 md:px-4 px-4 py-2 rounded uppercase  focus:outline-none"
+                : "cursor-pointer bg-green-800 shadow-md text-sm text-white font-bold py-0 md:px-4 px-4 py-2 hover:bg-green-600 rounded uppercase"
+            }
+            onClick={
+              rating > 0
+                ? () => addRating(rating)
+                : () => alert("Please select a rating before submitting")
+            }
+          >
+            Submit review
+          </button>
+        </div>
+        <div className="pl-4 font-medium flex">
+          <FaStar size={20} color="#ffc107" />
+          <div className="pl-2">{roundedReviewAverage}</div>
+          <div className="pl-2">({spot.reviews.length} reviews)</div>
+        </div>
       </div>
-
-      <div className="pl-4 font-medium flex">
-        <FaStar size={20} color="#ffc107" />
-        <div className="pl-2">{roundedReviewAverage}</div>
-        <div className="pl-2">({spot.reviews.length} reviews)</div>
-      </div>
-    </div>
+    </>
   );
 };
 
